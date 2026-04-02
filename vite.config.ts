c837af6663f1d108c64398f519b05acd167066c3
inobit <inoechoc@gmail.com>
2026-04-02 10:57:02 +0800

    chore: 初始化项目结构和配置

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      formats: ['es'],
+      fileName: 'index',
+    },
+    rollupOptions: {
+      external: [/node_modules/],
+      output: {
+        preserveModules: true,
+        preserveModulesRoot: 'src',
+      },
+    },
+    target: 'node20',
+    minify: false,
+    sourcemap: true,
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+});
